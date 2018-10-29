4bef72ba9153ae4719cd6c6f0ddfc8438ff03787
Arturo McGill <arturomcgill@gmail.com>
2018-10-29 17:50:55 -0400

    Initial empty activity

---
+++ app/src/main/java/edu/umd/arturomcgill/healcity/HealCityMainActivity.java
@@ -0,0 +1,13 @@
+package edu.umd.arturomcgill.healcity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HealCityMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_heal_city_main);
+    }
+}
